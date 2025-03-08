a5bbe4ade6e9211ee8c67dde1a5a0fc35b0f40e9
Chirakulal <chirashreelk@gmail.com>
2025-03-08 11:57:07 +0530

    Adding files

---
+++ BankRunner.java
@@ -0,0 +1,72 @@
+class BankRunner{
+	public static void main(String[] args){
+		double accountno1 = 117384912;
+		double balance1 = Bank.account(accountno1);
+		System.out.println("The account number "+accountno1+" has a balance of "+balance1);
+		
+		double accountno2 = 536274839;
+		double balance2 = Bank.account(accountno2);
+		System.out.println("The account number "+accountno2+" has a balance of "+balance2);
+		
+		double accountno3 = 362748392;
+		double balance3 = Bank.account(accountno3);
+		System.out.println("The account number "+accountno3+" has a balance of "+balance3);
+		
+		double accountno4 = 26374728;
+		double balance4 = Bank.account(accountno4);
+		System.out.println("The account number "+accountno4+" has a balance of "+balance4);
+		
+		double accountno5 = 35482937;
+		double balance5 = Bank.account(accountno5);
+		System.out.println("The account number "+accountno5+" has a balance of "+balance5);
+		
+		double accountno6 = 546378293;
+		double balance6 = Bank.account(accountno6);
+		System.out.println("The account number "+accountno6+" has a balance of "+balance6);
+		
+		double accountno7 = 73827492;
+		double balance7 = Bank.account(accountno7);
+		System.out.println("The account number "+accountno7+" has a balance of "+balance7);
+		
+		double accountno8 = 362637582;
+		double balance8 = Bank.account(accountno8);
+		System.out.println("The account number "+accountno8+" has a balance of "+balance8);
+		
+		double accountno9 = 13834922;
+		double balance9 = Bank.account(accountno9);
+		System.out.println("The account number "+accountno9+" has a balance of "+balance9);
+		
+		double accountno10 = 1526387458;
+		double balance10 = Bank.account(accountno10);
+		System.out.println("The account number "+accountno10+" has a balance of "+balance10);
+		
+		double accountno11 = 927361572;
+		double balance11 = Bank.account(accountno11);
+		System.out.println("The account number "+accountno11+" has a balance of "+balance11);
+		
+		double accountno12 = 64375245;
+		double balance12 = Bank.account(accountno12);
+		System.out.println("The account number "+accountno12+" has a balance of "+balance12);
+		
+		double accountno13 = 37817248;
+		double balance13 = Bank.account(accountno13);
+		System.out.println("The account number "+accountno13+" has a balance of "+balance13);
+		
+		double accountno14 = 272543562;
+		double balance14 = Bank.account(accountno14);
+		System.out.println("The account number "+accountno14+" has a balance of "+balance14);
+		
+		double accountno15 = 648327984;
+		double balance15 = Bank.account(accountno15);
+		System.out.println("The account number "+accountno15+" has a balance of "+balance15);
+		
+		double accountno16 = 88326428;
+		double balance16 = Bank.account(accountno16);
+		System.out.println("The account number "+accountno16+" has a balance of "+balance16);
+		
+		double accountno17 = 83928863;
+		double balance17 = Bank.account(accountno17);
+		System.out.println("The account number "+accountno17+" has a balance of "+balance17);
+		
+	}
+}
